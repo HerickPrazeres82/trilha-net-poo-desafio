92d63a765b6dfb8798a87cc6455dd73180d4cc13
Herick Prazeres <herick_prazeres@hotmail.com>
2025-04-28 00:25:55 -0300

    Desafio Smartphone DIO - Finalizado em 28/04/2025

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("4899998877", "NK54", "AB152DE", 64);
+
+Iphone iphone = new Iphone("4899998888", "IP15", "JJ122FF", 256);
+
+Console.WriteLine("Smartphone Nokia");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone");
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
+
+Console.ReadKey();
